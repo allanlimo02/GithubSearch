2e23907359ecdbc467bef8286bf180c97c1a972a
allanlimo02 <allan.limo@student.moringaschool.com>
2021-08-29 20:04:19 +0300

    updated services

--- src/app/servicepage/service.service.ts
+++ src/app/servicepage/service.service.ts
@@ -11,6 +11,7 @@ import { Search } from '../search';
 export class ServiceService {
   
   uname!:string;
+  repoName!:string
   
 
   getUser(uname:any){
@@ -18,16 +19,10 @@ export class ServiceService {
     .pipe(((response: any) => response));
   }
   getRepos(repoName:any){
-    return this.Http.get("https://api.github.com/users/"+ repoName +"+/repos?access_token=" + environment.githubApi)
+    return this.Http.get("https://api.github.com/users/"+ repoName +"/repos?access_token=" + environment.githubApi)
     .pipe(((response: any) => response));
 
   }
 
   constructor(private Http:HttpClient) { }
-
-  public getProfile(uname: any){
-    let profileUrl=`${environment.githbUrl}+${uname}?apiKey=${environment.githubApi}`;
-    let repoUrl=`${environment.githbUrl}+${uname}+/repos/?apiKey=${environment.githubApi}`;
-
-  }
 }
